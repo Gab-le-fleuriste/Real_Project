e1361ef110ed0f707c5d633cd5a71f32be857299
Gab-le-fleuriste <gabriel.bilodeau.work@gmail.com>
2023-04-25 15:32:58 -0400

    allo

---
+++ StoreFrontConsole/src/main/java/com/example/storefrontconsole/StoreFrontConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.storefrontconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreFrontConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreFrontConsoleApplication.class, args);
+    }
+
+}
---
+++ StoreFrontConsole/src/test/java/com/example/storefrontconsole/StoreFrontConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storefrontconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFrontConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
